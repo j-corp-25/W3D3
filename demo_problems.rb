cc9a45c652fe46560073b8939d04f74c8b951a2e
Jordy <jordycorporan95@gmail.com>
2023-05-17 09:39:46 -0400

    working on recursion problems in class

---
+++ demo_problems.rb
@@ -0,0 +1,42 @@
+def iter_sum(n)
+  sum = 0
+  (1..n).each {|i| sum += i}
+ return sum
+
+end
+p iter_sum(3)
+p iter_sum(7)
+p iter_sum(10)
+
+def rec_sum(n)
+  return n if n == 0
+  return n if n == 1
+  return n + iter_sum(n - 1)
+
+end
+p rec_sum(3)
+p rec_sum(7)
+p rec_sum(10)
+p rec_sum(10000)
+
+rec_sum(3)
+3 + rec_sum(2)
+3 + 2 + rec_sum(1)
+3 + 2 + 1 + rec_sum(0)
+3 + 2 + 1 + 0
+
+
+def iter_reverse(str)
+  new_str = ""
+  str.each_char do |char|
+    new_str = char + new_str
+  end
+  return new_str
+end
+
+
+
+def rec_reverse(str)
+  return str if str.length <= 1
+  rec_reverse(str[1..-1]) + str[0]
+end
