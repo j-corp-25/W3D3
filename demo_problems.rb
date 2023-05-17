088b2f4dbea0d7341efb3b665e5a9c16a68b193a
Jordy <jordycorporan95@gmail.com>
2023-05-17 10:55:34 -0400

    pairing time

--- demo_problems.rb
+++ demo_problems.rb
@@ -109,4 +109,17 @@ p new_all_fibs(5)
 p new_all_fibs(20)
 
 
-def pascal_triangle
+def pascal_triangle(n)
+  return [1] if n == 0
+  return [1, 1] if n == 1
+  last_row = pascal_row(n - 1)
+  next_row = [1]
+
+  (0...last_row.length - 1).each do |i|
+    next_row << last_row[i] + last_row[i + 1]
+  end
+  
+
+  next_row << 1
+
+end
